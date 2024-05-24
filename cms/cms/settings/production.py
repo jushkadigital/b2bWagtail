803f825d01dce14829b22b405bc212289e44232f
chamba <urgosxd@gmail.com>
2024-05-24 10:32:18 -0700

    aoe

--- cms/cms/settings/production.py
+++ cms/cms/settings/production.py
@@ -17,7 +17,7 @@ DATABASES = {
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
 # and renames the files with unique names for each version to support long-term caching
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # try:
 #     from .local import *
