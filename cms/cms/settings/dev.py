5e7889b6b474652db2d709bfcb81ff6cd120c517
chamba <urgosxd@gmail.com>
2024-05-24 06:35:18 -0700

    aoe

---
+++ cms/cms/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-a3s&*@v_dq7v_s(@ivicmiq*7qg)ecux1wu_^)5*l-vb@-aif%"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
